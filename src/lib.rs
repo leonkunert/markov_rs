a4e67b5d53d1c2231d85d4d4ce60abe98aebe230
Leon Kunert <info@leonkunert.de>
2015-01-03 23:53:23 +0100

    Upgraded to latest rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
 use std::io::File;
-use std::collections::TreeMap;
+use std::collections::BTreeMap;
+
+fn main() {
+    let chain = load_chain_from_file(&Path::new(".test"));
+    println!("Chain {}", chain);
+}
+
 
 fn load_content_from_file(path: &Path) -> String {
     match File::open(path).read_to_string() {
@@ -8,32 +14,29 @@ fn load_content_from_file(path: &Path) -> String {
     }
 }
 
-#[test]
-fn test_load_from_file() {
-    let file_content = load_content_from_file(&Path::new("./test"));
-    let mut table: TreeMap<String, TreeMap<String, uint>> = TreeMap::new();
+fn load_chain_from_file(path: &Path) -> BTreeMap<String, BTreeMap<String, uint>> {
+    let file_content = load_content_from_file(path);
+    let mut table: BTreeMap<String, BTreeMap<String, uint>> = BTreeMap::new();
     let mut previous_word: String = "".to_string();
     for word in file_content.split_str(" ".as_slice()) {
         if !table.contains_key(&word.into_string()) {
-            table.insert(word.to_string(), TreeMap::new());
+            table.insert(word.to_string(), BTreeMap::new());
         }
         if previous_word.as_slice() != "".as_slice() {
             let mut val = table.get_mut(&previous_word.to_string()).unwrap();
             if val.contains_key(&word.to_string()) {
                 let count = val.remove(&word.to_string()).unwrap();
-                val.insert(word.to_string(), count.add(&1u));
+                val.insert(word.to_string(), count + 1u);
             } else {
                 val.insert(word.to_string(), 1u);
             }
         }
         previous_word = word.to_string();
     }
-    for (key, value) in table.iter(){
-        for (key_key, value_key) in value.iter() {
-            if value_key > &3u {
-                println!("\"{} {}\" occours {} times", key, key_key, value_key);
-            }
-        }
-    }
-    assert!(false);
+    table
+}
+
+fn iter_for_word(word: String, chain: BTreeMap<String, BTreeMap<String, uint>>) {
+    println!("String {}", word);
+
 }
